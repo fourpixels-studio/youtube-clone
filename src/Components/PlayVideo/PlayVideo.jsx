d3d78d21dcca980989e6f7b031fec718bcd54cb5
Four Pixels <121856163+4ourpixels@users.noreply.github.com>
2024-02-14 12:09:29 +0300

    Third commit 🚀

--- src/Components/PlayVideo/PlayVideo.jsx
+++ src/Components/PlayVideo/PlayVideo.jsx
@@ -1,22 +1,68 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./PlayVideo.css";
-import video1 from "../../assets/video.mp4";
 import like from "../../assets/like.png";
 import dislike from "../../assets/dislike.png";
 import share from "../../assets/share.png";
 import save from "../../assets/save.png";
-import jack from "../../assets/jack.png";
-import user_profile from "../../assets/user_profile.jpg";
+import { API_KEY, value_converter } from "../../data";
+import moment from "moment";
+import { useParams } from "react-router-dom";
+
 const PlayVideo = () => {
+  const { videoId } = useParams();
+  const [apiData, setApiData] = useState(null);
+  const [channelData, setChannelData] = useState(null);
+  const [commentData, setCommentData] = useState([]);
+
+  const fetchVideoData = async () => {
+    // Fetching the video's data
+    const videoDetails_url = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=${videoId}=${API_KEY}`;
+    await fetch(videoDetails_url)
+      .then((res) => res.json())
+      .then((data) => setApiData(data.items[0]));
+  };
+
+  const fetchOtherData = async () => {
+    // Fetching the channel data
+    const channelData_url = `https://youtube.googleapis.com/youtube/v3/channels?part=snippet%2CcontentDetails%2Cstatistics&id=${videoId}&key=${API_KEY}`;
+    await fetch(channelData_url)
+      .then((res) => res.json())
+      .then((data) => setChannelData(data.items));
+
+    // Fetching the comment data
+    const comment_url = `https://youtube.googleapis.com/youtube/v3/commentThreads?part=snippet%2Creplies&maxResults=50&videoId=${videoId}&key=${API_KEY}`;
+    await fetch(comment_url)
+      .then((res) => res.json())
+      .then((data) => setCommentData(data.items[0]));
+  };
+
+  useEffect(() => {
+    fetchVideoData();
+  }, [videoId]);
+
+  useEffect(() => {
+    fetchOtherData();
+  }, [apiData]);
+
   return (
     <div className="play-video">
-      <video src={video1} controls autoPlay muted loop></video>
-      <h3>Highjacked season 08 | Trap music | Best of 2020-2024 by DJ G400</h3>
+      {/* <video src={video1} controls autoPlay muted loop></video> */}
+      <iframe
+        src={`https://www.youtube.com/embed/${videoId}?autoplay=1`}
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowfullscreen
+      ></iframe>
+      <h3>{apiData ? apiData.snippet.title : "Title here"}</h3>
       <div className="play-video-info">
-        <p>40,000 Views &bull; 4 days ago</p>
+        <p>
+          {apiData ? value_converter(apiData.statistics.viewCount) : "4k"} Views
+          &bull; {apiData ? moment(apiData.snippet.publishedAt).fromNow() : ""}
+        </p>
         <div>
           <span>
-            <img src={like} alt="" /> 404
+            <img src={like} alt="" />
+            {apiData ? value_converter(apiData.statistics.likeCount) : 404}
           </span>
           <span>
             <img src={dislike} alt="" /> 4
@@ -31,113 +77,58 @@ const PlayVideo = () => {
         <hr />
       </div>
       <div className="publisher">
-        <img src={jack} alt="" />
+        <img
+          src={channelData ? channelData.snippet.thumbnails.default.url : ""}
+          alt=""
+        />
         <div>
-          <p>DJ G400</p>
-          <span>4M Subscribers</span>
+          <p>{apiData ? apiData.snippet.channelTittle : "Channel Title"}</p>
+          <span>
+            {channelData
+              ? value_converter(channelData.statistics.subscriberCount)
+              : "4M"}
+            Subscribers
+          </span>
         </div>
         <button>Subscribe</button>
       </div>
       <div className="vid-decription">
-        <p>Causing 400 problems for these copycat DJS</p>
+        <p>
+          {apiData
+            ? apiData.snippet.description.slice(0, 250)
+            : "Description goes here"}
+        </p>
         <hr />
-        <h4>400 Comments</h4>
-        <div className="comment">
-          <img src={user_profile} alt="" />
-          <div>
-            <h3>
-              Arap Trap <span>1 day ago</span>
-            </h3>
-            <p>
-              Give these DJs some uniform cause I'm taking them back to school
-            </p>
-            <div className="comment-action">
-              <img src={like} alt="" />
-              <span>440</span>
-              <img src={dislike} alt="" />
+        <h4>
+          {apiData ? value_converter(apiData.statistics.commentCount) : 404}{" "}
+          Comments
+        </h4>
+        {commentData.map((item, index) => {
+          return (
+            <div key={index} className="comment">
+              <img
+                src={item.snippet.topLevelComment.snippet.authorProfileImageUrl}
+                alt=""
+              />
+              <div>
+                <h3>
+                  {item.snippet.topLevelComment.snippet.authorDisplayName}
+                  <span>1 day ago</span>
+                </h3>
+                <p>{item.snippet.topLevelComment.snippet.textDisplay}</p>
+                <div className="comment-action">
+                  <img src={like} alt="" />
+                  <span>
+                    {value_converter(
+                      item.snippet.topLevelComment.snippet.likeCount
+                    )}
+                  </span>
+                  <img src={dislike} alt="" />
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
-        <div className="comment">
-          <img src={user_profile} alt="" />
-          <div>
-            <h3>
-              Arap Trap <span>1 day ago</span>
-            </h3>
-            <p>
-              Give these DJs some uniform cause I'm taking them back to school
-            </p>
-            <div className="comment-action">
-              <img src={like} alt="" />
-              <span>440</span>
-              <img src={dislike} alt="" />
-            </div>
-          </div>
-        </div>
-        <div className="comment">
-          <img src={user_profile} alt="" />
-          <div>
-            <h3>
-              Arap Trap <span>1 day ago</span>
-            </h3>
-            <p>
-              Give these DJs some uniform cause I'm taking them back to school
-            </p>
-            <div className="comment-action">
-              <img src={like} alt="" />
-              <span>440</span>
-              <img src={dislike} alt="" />
-            </div>
-          </div>
-        </div>
-        <div className="comment">
-          <img src={user_profile} alt="" />
-          <div>
-            <h3>
-              Arap Trap <span>1 day ago</span>
-            </h3>
-            <p>
-              Give these DJs some uniform cause I'm taking them back to school
-            </p>
-            <div className="comment-action">
-              <img src={like} alt="" />
-              <span>440</span>
-              <img src={dislike} alt="" />
-            </div>
-          </div>
-        </div>
-        <div className="comment">
-          <img src={user_profile} alt="" />
-          <div>
-            <h3>
-              Arap Trap <span>1 day ago</span>
-            </h3>
-            <p>
-              Give these DJs some uniform cause I'm taking them back to school
-            </p>
-            <div className="comment-action">
-              <img src={like} alt="" />
-              <span>440</span>
-              <img src={dislike} alt="" />
-            </div>
-          </div>
-        </div>
-        <div className="comment">
-          <img src={user_profile} alt="" />
-          <div>
-            <h3>
-              Arap Trap <span>1 day ago</span>
-            </h3>
-            <p>
-              Give these DJs some uniform cause I'm taking them back to school
-            </p>
-            <div className="comment-action">
-              <img src={like} alt="" />
-              <span>440</span>
-              <img src={dislike} alt="" />
-            </div>
-          </div>
-        </div>
+          );
+        })}
       </div>
     </div>
   );
